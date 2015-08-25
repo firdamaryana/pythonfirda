be50b4ceeaebcbc1c0756372f240ccf43dfcf2d7
firda <firda@firda-NEON-RNE.(none)>
2015-08-25 09:26:09 +0700

    first commit

---
+++ firda.py
@@ -0,0 +1,5 @@
+print ("program penghitung luas segitiga")
+panjang=input("masukkan panjang :")
+tinggi=input("masukkan tinggi :")
+luas = (panjang*tinggi)/2
+print "luas segitiga tersebut adalah : {0}".format(luas)
