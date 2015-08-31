34fe06a0ed0a3d9b1990febc2a5ed5b92b7b2e8b
Firda <firda_maryana94@yahoo.com>
2015-08-31 11:37:29 +0700

    Belajar For

---
+++ meet4/for.py
@@ -0,0 +1,10 @@
+#Deret 1 2 3 4 5
+for a in range (1 , 6):
+    print a,
+
+print
+print
+
+for a in range (0 , 10, 2):
+    print a,
+
---
+++ meet4/for1.py
@@ -0,0 +1,10 @@
+t=input("Masukkan Perulangan?")
+a=0
+for a in range (1, t+1):
+      print a,
+
+print
+print
+
+for a in range (5, t-1):
+      print a,
