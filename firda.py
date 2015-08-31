0a7522d1191bc92360a2a7f837f9a316ececc59d
Firda <firda_maryana94@yahoo.com>
2015-08-31 10:54:40 +0700

    Rapihin Folder

--- firda.py
+++
@@ -1,5 +0,0 @@
-print ("program penghitung luas segitiga")
-panjang=input("masukkan panjang :")
-tinggi=input("masukkan tinggi :")
-luas = (panjang*tinggi)/2
-print "luas segitiga tersebut adalah : {0}".format(luas)
